0072b3f7901ed6454369445166ebea2bdc2d690d
Micaela Sarotto <micaelasarotto@gmail.com>
2024-09-17 20:42:42 -0300

    funcionando

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
+let cantidad = document.getElementById('cantidad');
+let boton = document.getElementById('generar');
+let contrasena = document.getElementById('contrasena');
 
+const cadenaCaracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890';
 
+function generar() {
 
+    let numeroDigitado = parseInt(cantidad.value)
 
+    if (numeroDigitado < 8) {
+        alert("La cantidad de caracteres tiene que ser mayor que 8");
+    }
 
+    let password = '';
 
+    for (let i = 0; i < numeroDigitado; i++) {
+        let caracterAleatorio = cadenaCaracteres[Math.floor(Math.random() * cadenaCaracteres.length)];
+        console.log(caracterAleatorio);
 
-
-
+        password += caracterAleatorio;
+    }
+    contrasena.value= password;
+}
